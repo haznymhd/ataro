30072922ffd2c8ac4a0c432b4ccae8924a3b44ef
Hasni <145745688+haznymhd@users.noreply.github.com>
2025-02-11 23:22:21 +0530

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Home = () => {
   return (
     <div className="min-h-screen bg-gray-200 flex flex-col items-center justify-center relative">
-      {/* Header with Logo */}
+
       <header className="absolute top-4 left-4">
         <img src="valkoware1.png" alt="ValkoWare Logo" className="w-40 ml-5 mt-5" />
       </header>
       
-      {/* Left-side Rotated Intro Logo */}
+
       <div className="absolute right-[-40px] top-1/2 transform -translate-y-[300px] -rotate-90">
         <img 
           src="intro_logo.png" 
@@ -17,7 +17,7 @@ const Home = () => {
         />
       </div>
       
-      {/* Centered Background Logo with Opacity */}
+
       <div className="absolute inset-0 flex items-center justify-center">
         <img 
           src="valkoware.png" 
@@ -26,7 +26,7 @@ const Home = () => {
         />
       </div>
       
-      {/* Centered Image Responsive */}
+
       <div className="flex flex-col items-center justify-center z-10 w-full">
         <img 
           src="Mobile Smartphone Mockup 03.png" 
@@ -34,13 +34,13 @@ const Home = () => {
           className="w-[80%] h-auto max-w-none md:w-[70%] lg:w-[30%]" 
         />
         
-        {/* Contact Information */}
+
 {/*         <p className="mt-4 md:mb-[-80px] text-gray-700 text-sm md:text-base font-medium text-center">
           Point of Sale (POS) Systems | Software Development | SEO Optimization | Cloud Deployment | UI/UX Design
         </p> */}
       </div>
 
-      {/* Footer with Services */}
+
       <footer className="absolute md:bottom-4 bottom-[100px] text-center text-gray-600 text-sm ">
       Point of Sale (POS) Systems | Software Development | SEO Optimization | Cloud Deployment | UI/UX Design <br /><br />info@valkoware.com | +94 71 777 7668 
       </footer>
